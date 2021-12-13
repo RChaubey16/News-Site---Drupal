e72752e58253fba457132c84d52f10faf25c4623
RChaubey16 <ruturajchaubey16@gmail.com>
2021-12-13 13:56:16 +0530

    #1: Added interactivity to search bar

--- themes/custom/demo/js/search.js
+++ themes/custom/demo/js/search.js
@@ -1,2 +1,17 @@
-var searchBtn = document.getElementById("edit-submit");
-// searchBtn.value ="SEARCH";
+var searchBox = document.getElementById("search__box");
+
+var searchIcon = document.getElementById("block-views-block-search-icon-block-1");
+
+var searchCrossIcon = document.getElementById(
+  "block-views-block-search-cross-icon-block-1"
+);
+
+searchIcon.addEventListener("click", function(){
+   console.log("CLick");
+   searchBox.style.display = "block";
+})
+
+searchCrossIcon.addEventListener("click", function () {
+  console.log("Clicked");
+  searchBox.style.display = "none";
+});
