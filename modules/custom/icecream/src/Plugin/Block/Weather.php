52472a19b8ad964aae8a801dfe9d070b21860039
RChaubey16 <ruturajchaubey16@gmail.com>
2022-01-27 14:45:27 +0530

    Added search and styled the search page

--- modules/custom/icecream/src/Plugin/Block/Weather.php
+++ modules/custom/icecream/src/Plugin/Block/Weather.php
@@ -27,6 +27,24 @@ class Weather
         
     }   
 
+    public function getPollutionIndex($lon, $lat){
+        $apiKey = "1f5cb8219df8e3067dd65255da42dd9f";
+        $googleApiUrl = "https://api.openweathermap.org/data/2.5/air_pollution?lat=".$lat."&lon=".$lon."&appid=".$apiKey;
+
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_URL, $googleApiUrl);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+        curl_setopt($ch, CURLOPT_VERBOSE, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        $response = curl_exec($ch);
+        curl_close($ch);
+
+        return $response;
+    }
+
     public function name(){
         return "Raj";
     }
--- modules/custom/icecream/src/Plugin/Block/WeatherBlock.php
+++ modules/custom/icecream/src/Plugin/Block/WeatherBlock.php
@@ -27,6 +27,20 @@ class WeatherBlock extends BlockBase
 
         $config = $this->getConfiguration();
         $city = $config['weather_block_mode']['city'];
+
+        $options = array(
+            'weather' => $this->t('temperature'),
+            'pollution' => $this->t('pollution'),
+        );
+
+        $form['weather_or_pollution'] = [
+            '#type' => 'radios',
+            '#title' => $this->t('Choose on of the following'),
+            '#description' => $this->t('Will display pollution rate or current weather of the city'),
+            '#options' => $options,
+            '#default_value' => $options['pollution'],
+        ];
+
         $form['Weather'] = [
             '#type' => 'textfield',
             '#title' => $this->t('City'),
@@ -34,6 +48,8 @@ class WeatherBlock extends BlockBase
             '#name' => "city",
             '#value' => $city,
         ];
+
+
         return $form;
     }
 
@@ -47,7 +63,7 @@ class WeatherBlock extends BlockBase
 
         $this->city = $form_state->getUserInput('city');
         $messenger = \Drupal::messenger();
-        $messenger->addMessage('Weather city accepted' . $this->city['city']);
+        // $messenger->addMessage('Weather city accepted' . $this->city['city']);
     }
 
     public function build()
@@ -55,13 +71,27 @@ class WeatherBlock extends BlockBase
 
         $config = $this->getConfiguration();
         $city = $config['weather_block_mode']['city'];
+        $typeOfData = $config['weather_block_mode']['settings']['weather_or_pollution'];
 
         $result = \Drupal::service('weather.service')->getWeather($city);
         $weatherData = json_decode($result);
 
-        return [
-            "#markup" => " " . $weatherData->name . " " . $weatherData->main->temp . " °C",
+        if ($typeOfData == "weather") {
+            return [
+                "#markup" => " " . $weatherData->name . " " . $weatherData->main->temp . " °C",
 
-        ];
+            ];
+        } elseif ($typeOfData == "pollution") {
+            $lon = $weatherData->coord->lon;
+            $lat = $weatherData->coord->lat;
+
+            $result = \Drupal::service('weather.service')->getPollutionIndex($lon, $lat);            
+            $pollutionData = json_decode($result);
+            $rate = $pollutionData->list[0]->components->co;
+
+            return[
+                "#markup" => " " . $weatherData->name . " Pollution " . $rate . " AQI",
+            ];
+        }
     }
 }
